91888ba9b78452729e4442783983a7b243ea9e78
Aryan Verma <aryan407012@gmail.com>
2026-03-27 22:00:48 +0530

    Create Home.js component for homepage

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the AR/VR App</h1>
+            <p>This is a simple homepage for the AR/VR application.</p>
+        </div>
+    );
+};
+
+export default Home;
